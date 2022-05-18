df31d780d12f87534e0a9d433b3b5164f9012661
arjun <arjun.mehta1001@gmail.com>
2022-05-17 23:07:53 -0600

    tailwind-styled-components configured for future usage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import tw from "tailwind-styled-components";
+import "../dist/output.css";
+
+const Helo = tw.div`
+    bg-red-800
+    h-screen
+`;
+
+
+import { 
+    BrowserRouter as Router, 
+    Route, 
+    Switch, 
+    Link, 
+    Redirect,
+} from "react-router-dom";
+
 
 const App = () => {
     return (
-        <h1>
-            Welcome to React App thats build using Webpack and Babel separately
-        </h1>
+        <Helo></Helo>
     )
 }
 
