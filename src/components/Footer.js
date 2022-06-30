4d840c95878d6c8f1df1997401408a201b4a7f44
arjun <arjun.mehta1001@gmail.com>
2022-06-29 23:42:11 -0600

    footer mostly complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,38 @@ import React from "react";
 import tw from "tailwind-styled-components";
 
 const FooterContainer = tw.div`
-    flex
     bg-[#0B3C49]
     text-white
-    flex-row
     w-max-[100vw]
     w-full
     h-28
+    py-8
 `;
 
 const FText = tw.p`
     p-2
     text-2xl
+    float-left
+    inline-block
+    pl-6
+`;
+
+const Anchor = tw.a`
+    p-2
+    text-2xl
+    float-right
+    inline-block
+    pr-6
 `;
 
 const Footer = () => {
     return (
         <FooterContainer>
-            <FText>Designed and Coded by Me (Arjun) 😎</FText>
-            {/* TODO: add Links floated to right */}
+            {/* TODO: add hover delay create components */}
+            <FText className="float-left inline-block pl-6">Designed and Coded by Me (Arjun) 😎</FText>
+            <Anchor href="https://github.com/ArjunMehta01">Github</Anchor>
+            <Anchor href="https://www.linkedin.com/in/arjunmehta01/">LinkedIn</Anchor>
+            {/* TODO maybe: https://devpost.com/arjun-mehta1001 */}
         </FooterContainer>
     );
 }
